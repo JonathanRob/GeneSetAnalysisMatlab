b74fdd4aadfc9f7039000b28911b59d563904ad8
Jonathan Robinson <jonrob@chalmers.se>
2020-01-22 23:21:35 +0700

    feat: GSAheatmap now able to handle multiple GSA results as an array
    plots non-directional and distinct-directional heatmaps

--- GSAheatmap.m
+++ GSAheatmap.m
@@ -1,4 +1,4 @@
-function [] = GSAheatmap(GSAres,adjusted,filterMethod,filterThresh,colorMax)
+function [] = GSAheatmap(GSAres,adjusted,filterMethod,filterThresh,colorMax,dirType)
 % GSAheatmap  Generate a heatmap to visualize GSA results.
 %
 %
@@ -45,11 +45,24 @@ function [] = GSAheatmap(GSAres,adjusted,filterMethod,filterThresh,colorMax)
 %                   on the heatmap colorbar.
 %                   (Default = maximum -log10 pvalue)
 %
+%   dirType         String specifying the type of gene set p-values to use
+%                   in the heatmap.
+%                   
+%                   'nondir'  use non-directional p-values
 %
-% Jonathan Robinson, 2020-01-19
+%                   'dir'     use distinct-directional p-values
+%
+%                   'both'    (Default) use both non- and distinct-
+%                             directional p-values (creates 2 figures)
+%
+%                   NOTE: This input is only used when GSAres is a cell
+%                   array of multiple GSA result structures.
+%
+%
+% Jonathan Robinson, 2020-01-22
 
 
-%% Initialize and organize data
+%% Handle inputs
 
 % assign default inputs
 if nargin < 2 || isempty(adjusted)
@@ -73,144 +86,321 @@ end
 if nargin < 5
     colorMax = [];
 end
-
-% extract p-value data from GSAres
-pValCols = {'p_distdn';'p_mixdn';'p_nondir';'p_mixup';'p_distup'};
-pValColNames = {'dist-down';'mix-down';'non-dir';'mix-up';'dist-up'};
-if ( adjusted )
-    pValCols = regexprep(pValCols,'p_','padj_');
-end
-[keep,pValColInds] = ismember(pValCols,GSAres.Properties.VariableNames);
-pValColInds = pValColInds(keep);  % in case some p-value types are missing
-pValColNames = pValColNames(keep);
-pData = table2array(GSAres(:,pValColInds));
-
-% set max color value if not specified
-if isempty(colorMax)
-    colorMax = max(-log10(pData(:)));
+if nargin < 6
+    dirType = 'both';
+elseif ismember(dirType,{'dir','distdir','dist-dir','dist.dir','directional'})
+    dirType = 'dir';
+elseif ismember(dirType,{'nondir','non-dir','non.dir','nondirectional','non-directional'})
+    dirType = 'nondir';
+else
+    error('dirType not recognized. Must be "nondir", "dir", or "both".');
 end
 
-% get rownames (gene-set names)
-pValRowNames = GSAres.GS_name;
+
+%% Extract and prepare p-value data
+
+if istable(GSAres)  % single GSA result table
+    
+    % extract p-value data from GSAres
+    table_cols = {'p_distdn';'p_mixdn';'p_nondir';'p_mixup';'p_distup'};
+    colnames = {'dist-down';'mix-down';'non-dir';'mix-up';'dist-up'};
+    if ( adjusted )
+        table_cols = regexprep(table_cols,'p_','padj_');
+    end
+    [keep,col_inds] = ismember(table_cols,GSAres.Properties.VariableNames);
+    col_inds = col_inds(keep);  % in case some p-value types are missing
+    colnames = colnames(keep);
+    pData = table2array(GSAres(:,col_inds));
+    
+    % set max color value if not specified
+    if isempty(colorMax)
+        colorMax = max(-log10(pData(:)));
+    end
+    
+    % assign rownames (gene-set names)
+    rownames = GSAres.GS_name;
+    
+    % filter rows from p-value matrix
+    keep_rows = filter_pData(pData,filterMethod,filterThresh);
+    if isempty(keep_rows)
+        error('No gene sets passed the filter - try relaxing the constraints.');
+    end
+    pData = pData(keep_rows,:);
+    rownames = rownames(keep_rows);
+    
+    % log-transform p-values
+    log_pData = -log10(pData);
+    
+    % calculate "directionality score" of each row
+    % dir.score = (p.mix.up*(p.non.dir + p.dist.up) - p.mix.dn*(p.non.dir + p.dist.dn)) / (2*max(p)^2)
+    if size(log_pData,2) == 5
+        dir_score = (log_pData(:,4).*(log_pData(:,3) + log_pData(:,5)) - log_pData(:,2).*(log_pData(:,3) + log_pData(:,1)))./(2*max(log_pData(:)).^2);
+    elseif all(strcmp(colnames,{'dist-down';'non-dir';'dist-up'})) || ...
+            all(strcmp(colnames,{'mix-down';'non-dir';'mix-up'}))
+        % if mix-directional or distinct-directional p-values are missing
+        dir_score = (log_pData(:,3) - log_pData(:,1)).*log_pData(:,2);
+    end
+    [~,sort_ind] = sort(dir_score);
+    
+    % sort rows of pData by directionality score
+    log_pData = log_pData(sort_ind,:);
+    rownames = rownames(sort_ind);
+    
+elseif iscell(GSAres)  % array of multiple GSA result tables
+    
+    % find gene sets that are not present in all GSAres tables
+    GSnames = getFieldContentsFromArray(GSAres,'GS_name');
+    GSnames_intersect = multiIntersect(GSnames{:});
+    GSnames_union = unique(vertcat(GSnames{:}));
+    GSremoved = setdiff(GSnames_union, GSnames_intersect);
+    if ~isempty(GSremoved)
+        fprintf('\nWarning! The following gene sets are not present in all GSAres tables and were therefore excluded:\n');
+        fprintf('\t%s\n',GSremoved{1:min(5,numel(GSremoved))});  % only print first 5
+        if numel(GSremoved) > 5
+            fprintf('\t...and %u more.\n\n',numel(GSremoved)-5);
+        end
+    end
+    
+    [pData_nondir,pData_distdn,pData_distup] = deal(zeros(numel(GSnames_intersect),numel(GSAres)));
+    for i = 1:numel(GSAres)
+        [~,ind] = ismember(GSnames_intersect, GSAres{i}.GS_name);
+        if ~(adjusted)
+            pData_nondir(:,i) = GSAres{i}.p_nondir(ind);
+            pData_distdn(:,i) = GSAres{i}.p_distdn(ind);
+            pData_distup(:,i) = GSAres{i}.p_distup(ind);
+        else
+            pData_nondir(:,i) = GSAres{i}.padj_nondir(ind);
+            pData_distdn(:,i) = GSAres{i}.padj_distdn(ind);
+            pData_distup(:,i) = GSAres{i}.padj_distup(ind);
+        end
+    end
+    
+    % log-transform p-values, and merge directional p-values
+    log_pData_nondir = -log10(pData_nondir);
+    pData_distdir = min(pData_distdn, pData_distup);
+    log_pData_distdir = -log10(pData_distdir);
+    
+    % if p_distdn < p_distup, then switch the sign (make negative)
+    log_pData_distdir(pData_distdn < pData_distup) = -log_pData_distdir(pData_distdn < pData_distup);
+    
+    % set max color value if not specified
+    if isempty(colorMax)
+        colorMax = max([log_pData_nondir(:); abs(log_pData_distdir(:))]);
+    end
+    
+    % assign rownames (gene-set names)
+    rownames = GSnames_intersect;
+    
+    % filter rows from non-directional p-value matrix
+    if ~strcmp(dirType,'dir')
+        keep_rows = filter_pData(pData_nondir,filterMethod,filterThresh);
+        if isempty(keep_rows)
+            if strcmp(dirType,'both')
+                warning('No gene sets passed the filter on non-directional p-values - plot will not be shown!');
+            elseif strcmp(dirType,'nondir')
+                error('No gene sets passed the filter on non-directional p-values - try relaxing the constraints.');
+            end
+        end
+    else
+        keep_rows = [];
+    end
+    log_pData_nondir = log_pData_nondir(keep_rows,:);
+    rownames_nondir = rownames(keep_rows);
+    
+    % filter rows from distinct-directional p-value matrix
+    if ~strcmp(dirType,'nondir')
+        keep_rows = filter_pData(pData_distdir,filterMethod,filterThresh);
+        if isempty(keep_rows)
+            if strcmp(dirType,'both')
+                warning('No gene sets passed the filter on distinct-directional p-values - plot will not be shown!');
+            elseif strcmp(dirType,'nondir')
+                error('No gene sets passed the filter on distinct-directional p-values - try relaxing the constraints.');
+            end
+        end
+    else
+        keep_rows = [];
+    end
+    log_pData_distdir = log_pData_distdir(keep_rows,:);
+    rownames_distdir = rownames(keep_rows);
+    
+else
+    error('GSAres is in an unrecognized format. Should be a table, or a cell array of tables.');
+end
 
 
-% remove rows from data according to filtering method
+%% Generate heatmap
+
+if istable(GSAres)
+    
+    % define custom colormap
+    hotcmap = flipud(hot(120));
+    cmap_down = custom_cmap([0.1 0.1 0.8;0 0.7 0.9;1 1 1],100,[0.25;0.4;0.35]);
+    cmap_nondir = custom_cmap([0 0 0;1 1 1]);
+    cmap_up = hotcmap(1:100,:);
+    
+    % Columns will be colored according to their directionality.
+    % Columns 1 and 2 (p dist.dir.dn and mix.dir.dn) are colored according
+    % to the first colormap in CMAP, column 3 (p non.dir) is colored
+    % according to the second colormap in CMAP, and columns 4 and 5
+    % (p mix.dir.up and dist.dir.up) are colored according to the third
+    % color in CMAP.
+    cmap = [cmap_down; cmap_nondir; cmap_up];
+    if length(colnames) == 5
+        pData(:,1:2) = min(pData(:,1:2),0.999999*colorMax);
+        pData(:,3)   = min(pData(:,3) + 1.000001*colorMax,1.999999*colorMax);
+        pData(:,4:5) = min(pData(:,4:5) + 2.000001*colorMax,2.999999*colorMax);
+    elseif length(colnames) == 3
+        pData(:,1) = min(pData(:,1),0.999999*colorMax);
+        pData(:,2) = min(pData(:,2) + 1.000001*colorMax,1.999999*colorMax);
+        pData(:,3) = min(pData(:,3) + 2.000001*colorMax,2.999999*colorMax);
+    else
+        error('Incorrect number of columns.');
+    end
+    colorBounds = [0,3*colorMax];
+    
+    % trim very long gene set names
+    maxChar = 75;
+    longNames = cellfun(@(s) length(s) > maxChar, rownames);
+    rownames(longNames) = cellfun(@(s) [s(1:maxChar) '...'], rownames(longNames), 'UniformOutput', false);
+    
+    % generate heatmap
+    genHeatMap(pData, colnames, rownames, 'none', [], cmap, colorBounds, 'k');
+    
+    % scale plot horizontally to deal with too long or short gene set names
+    maxLength = max(cellfun(@length, rownames));
+    scaleX = min(max(0.5, 1-maxLength/50), 0.8);  % scale between 0.5x - 0.8x
+    pos = get(gca,'Position');
+    set(gca,'Position',pos .* [1 1 scaleX 1]);
+    
+    % add custom colorbars
+    % reduce figure height to make room for the colorbars
+    scaleY = min(0.85, 0.75 + size(pData,1)*0.002);
+    plotPos = get(gca,'Position') .* [1 1 1 scaleY];
+    set(gca,'Position', plotPos);
+    
+    % determine the position of the colorbars based on the plot position
+    cbar_width = max(0.02, 0.04 - size(pData,1)*0.001);  % scale width based on number of rows
+    c1Pos = [plotPos(1), plotPos(2)+plotPos(4)+0.02, plotPos(3), cbar_width];
+    c2Pos = [plotPos(1), plotPos(2)+plotPos(4)+0.02+1.5*cbar_width, plotPos(3), cbar_width];
+    c3Pos = [plotPos(1), plotPos(2)+plotPos(4)+0.02+3.0*cbar_width, plotPos(3), cbar_width];
+    
+    % add colorbars on top of plot
+    c1 = colorbar('Location','NorthOutside','Position',c1Pos,'TickLabels',[],'Limits',[0 colorMax]);
+    c2 = colorbar('Location','NorthOutside','Position',c2Pos,'TickLabels',[],'Limits',[0 colorMax]);
+    c3 = colorbar('Location','NorthOutside','Position',c3Pos,'FontSize',10,'Limits',[0 colorMax]);
+    
+    % label colorbar axis
+    xlabel(c3,'-log_1_0(p-value)','FontSize',10);
+    
+    % map each colorbar to a different p-value direction type
+    colormap(c1,repmat(cmap_down,3,1));
+    colormap(c2,repmat(cmap_nondir,3,1));
+    colormap(c3,repmat(cmap_up,3,1));
+    
+elseif iscell(GSAres)
+    
+    % retrieve or create column names
+    colnames = getFieldContentsFromArray(GSAres,'Properties.Description');
+    if all(cellfun(@isempty, colnames))
+        % if GSAres tables are not labeled, just use numbers (1, 2, 3, ...)
+        colnames = arrayfun(@num2str, (1:numel(GSAres))');
+    end
+
+    % plot non-directional p-value heatmap
+    if ismember(dirType,{'nondir','both'}) && ~isempty(log_pData_nondir)
+        % trim very long gene set names
+        maxChar = 75;
+        longNames = cellfun(@(s) length(s) > maxChar, rownames_nondir);
+        rownames_nondir(longNames) = cellfun(@(s) [s(1:maxChar) '...'], rownames_nondir(longNames), 'UniformOutput', false);
+        
+        % generate heatmap
+        genHeatMap(log_pData_nondir, colnames, rownames_nondir, 'both', ...
+            'euclid', custom_cmap('magma'), [0,colorMax], 'k');
+        
+        % scale plot horizontally to deal with too long or short gene set names
+        maxLength = max(cellfun(@length, rownames_nondir));
+        scaleX = min(max(0.5, 1-maxLength/50), 0.8);  % scale between 0.5x - 0.8x
+        pos = get(gca,'Position');
+        set(gca,'Position',pos .* [1 1 scaleX 1]);
+        
+        % add colorbar
+        c = colorbar('Location','NorthOutside');
+        xlabel(c,'-log_1_0(p.non.dir)','FontSize',10);
+    end
+    
+     % plot distinct-directional p-value heatmap
+    if ismember(dirType,{'distdir','both'}) && ~isempty(log_pData_distdir)
+        % trim very long gene set names
+        maxChar = 75;
+        longNames = cellfun(@(s) length(s) > maxChar, rownames_distdir);
+        rownames_distdir(longNames) = cellfun(@(s) [s(1:maxChar) '...'], rownames_distdir(longNames), 'UniformOutput', false);
+        
+        % generate heatmap
+        genHeatMap(log_pData_distdir, colnames, rownames_distdir, 'both', ...
+            'euclid', custom_cmap('redblue'), [-colorMax,colorMax], 'k');
+        
+        % scale plot horizontally to deal with too long or short gene set names
+        maxLength = max(cellfun(@length, rownames_distdir));
+        scaleX = min(max(0.5, 1-maxLength/50), 0.8);  % scale between 0.5x - 0.8x
+        pos = get(gca,'Position');
+        set(gca,'Position',pos .* [1 1 scaleX 1]);
+        
+        % add colorbar
+        c = colorbar('Location','NorthOutside');
+        xlabel(c,'Signed log_1_0(p.dist.dir)','FontSize',10);
+    end
+end
+
+
+end
+
+
+%% Additional functions
+
+function contents = getFieldContentsFromArray(array,field)
+% Retreive the contents of a given field from each structure in an array.
+% This function is necessary because dot indexing is not supported for cell
+% arrays of structures.
+if contains(field,'.')
+    % only works for one level of subfield (e.g., "field.subfield")
+    field = strsplit(field,'.');
+    contents = cellfun(@(x) {x.(field{1}).(field{2})}, array);
+else
+    contents = cellfun(@(x) {x.(field)}, array);
+end
+end
+
+
+function intersection = multiIntersect(varargin)
+% Determine intersection of two or more input vectors, arrays, etc.
+intersection = varargin{1};
+for i = 2:numel(varargin)
+    intersection = intersect(intersection, varargin{i});
+end
+end
+
+
+function keep_rows = filter_pData(pData,filterMethod,filterThresh)
+% Determine which rows of a p-value matrix to keep based on filter criteria
+
 switch filterMethod
     case 'pval'
         % keep rows that contain at least one entry below threshold p-val
-        keep_ind = find(any(pData < filterThresh,2));
+        keep_rows = find(any(pData < filterThresh,2));
     case 'top each'
         % keep the X lowest-pvalue-rows of each column, where X = filterThresh
         [~,sort_ind] = sort(pData);
-        keep_ind = unique(sort_ind(1:filterThresh,:));
+        keep_rows = unique(sort_ind(1:filterThresh,:));
     case 'top all'
         % keep the X lowest-pvalue-rows overall, where X = filterThresh
         [~,sort_ind] = sort(min(pData,[],2));
-        keep_ind = sort_ind(1:filterThresh);
+        keep_rows = sort_ind(1:filterThresh);
     case 'none'
         % keep all rows
-        keep_ind = 1:size(pData,1);
+        keep_rows = 1:size(pData,1);
     otherwise
         error('Invalid filtering method.');
 end
-if isempty(keep_ind)
-    error('No gene sets passed the filter - try relaxing the constraints.');
+
 end
-pData = pData(keep_ind,:);
-pValRowNames = pValRowNames(keep_ind);
-
-
-% log-transform p-values
-pData = -log10(pData);
-
-% calculate "directionality score" of each row
-% dir.score = (p.mix.up*(p.non.dir + p.dist.up) - p.mix.dn*(p.non.dir + p.dist.dn)) / (2*max(p)^2)
-if size(pData,2) == 5
-    dir_score = (pData(:,4).*(pData(:,3) + pData(:,5)) - pData(:,2).*(pData(:,3) + pData(:,1)))./(2*max(pData(:)).^2);
-elseif all(strcmp(pValColNames,{'dist-down';'non-dir';'dist-up'})) || ...
-        all(strcmp(pValColNames,{'mix-down';'non-dir';'mix-up'}))
-    % if mix-directional or distinct-directional p-values are missing
-    dir_score = (pData(:,3) - pData(:,1)).*pData(:,2);
-end
-[~,sort_ind] = sort(dir_score);
-
-% sort rows of pData by directionality score
-pData = pData(sort_ind,:);
-pValRowNames = pValRowNames(sort_ind);
-
-
-%% Generate heatmap
-
-% define custom colormap
-hotcmap = flipud(hot(120));
-cmap_down = custom_cmap([0.1 0.1 0.8;0 0.7 0.9;1 1 1],100,[0.25;0.4;0.35]);
-cmap_nondir = custom_cmap([0 0 0;1 1 1]);
-cmap_up = hotcmap(1:100,:);
-
-% Columns will be colored according to their directionality.
-% Columns 1 and 2 (p dist.dir.dn and mix.dir.dn) are colored according
-% to the first colormap in CMAP, column 3 (p non.dir) is colored
-% according to the second colormap in CMAP, and columns 4 and 5
-% (p mix.dir.up and dist.dir.up) are colored according to the third
-% color in CMAP.
-cmap = [cmap_down; cmap_nondir; cmap_up];
-if length(pValColNames) == 5
-    pData(:,1:2) = min(pData(:,1:2),0.999999*colorMax);
-    pData(:,3)   = min(pData(:,3) + 1.000001*colorMax,1.999999*colorMax);
-    pData(:,4:5) = min(pData(:,4:5) + 2.000001*colorMax,2.999999*colorMax);
-elseif length(pValColNames) == 3
-    pData(:,1) = min(pData(:,1),0.999999*colorMax);
-    pData(:,2) = min(pData(:,2) + 1.000001*colorMax,1.999999*colorMax);
-    pData(:,3) = min(pData(:,3) + 2.000001*colorMax,2.999999*colorMax);
-else
-    error('Incorrect number of columns.');
-end
-
-colorBounds = [0,3*colorMax];
-
-
-%% Generate heatmap
-
-% trim very long gene set names
-maxChar = 75;
-longNames = cellfun(@(s) length(s) > maxChar, pValRowNames);
-pValRowNames(longNames) = cellfun(@(s) [s(1:maxChar) '...'], pValRowNames(longNames), 'UniformOutput', false);
-
-% use custom function to generate heatmap
-genHeatMap(pData, pValColNames, pValRowNames, 'none', [], cmap, colorBounds, 'k');
-
-% scale plot horizontally to deal with too long or short gene set names
-maxLength = max(cellfun(@length, pValRowNames));
-scaleX = min(max(0.5, 1-maxLength/50), 0.8);  % scale between 0.5x - 0.8x
-pos = get(gca,'Position');
-set(gca,'Position',pos .* [1 1 scaleX 1]);
-
-
-%% add custom colorbars
-
-% reduce figure height to make room for the colorbars
-scaleY = min(0.85, 0.75 + size(pData,1)*0.002);
-plotPos = get(gca,'Position') .* [1 1 1 scaleY];
-set(gca,'Position', plotPos);
-
-% determine the position of the colorbars based on the plot position
-cbar_width = max(0.02, 0.04 - size(pData,1)*0.001);  % scale width based on number of rows
-c1Pos = [plotPos(1), plotPos(2)+plotPos(4)+0.02, plotPos(3), cbar_width];
-c2Pos = [plotPos(1), plotPos(2)+plotPos(4)+0.02+1.5*cbar_width, plotPos(3), cbar_width];
-c3Pos = [plotPos(1), plotPos(2)+plotPos(4)+0.02+3.0*cbar_width, plotPos(3), cbar_width];
-
-% add colorbars on top of plot
-c1 = colorbar('Location','NorthOutside','Position',c1Pos,'TickLabels',[],'Limits',[0 colorMax]);
-c2 = colorbar('Location','NorthOutside','Position',c2Pos,'TickLabels',[],'Limits',[0 colorMax]);
-c3 = colorbar('Location','NorthOutside','Position',c3Pos,'FontSize',10,'Limits',[0 colorMax]);
-
-% label colorbar axis
-xlabel(c3,'-log_1_0(p-value)','FontSize',10);
-
-% map each colorbar to a different p-value direction type
-colormap(c1,repmat(cmap_down,3,1));
-colormap(c2,repmat(cmap_nondir,3,1));
-colormap(c3,repmat(cmap_up,3,1));
-
-
 
